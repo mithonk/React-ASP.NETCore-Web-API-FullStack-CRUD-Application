02f6797e03393779fcb2990e53da49fb9d2347b7
mithonk <mitho9798@gmail.com>
2024-06-05 15:51:23 +0530

    05-06-2024

--- frontend-reactjs/src/components/StudentCrud.js
+++ frontend-reactjs/src/components/StudentCrud.js
@@ -1,4 +1,22 @@
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+
 function StudentCrud() {
+  const [id, setId] = useState('');
+  const [name, setName] = useState('');
+  const [course, setCourse] = useState('');
+  const [students, setStudents] = useState('');
+
+  useEffect(() => {
+    (async () => await Load())();
+  }, []);
+
+  async function Load() {
+    const result = await axios.get('');
+    setStudents(result.data);
+    console.log(result.data);
+  }
+
   return (
     <div>
       <h1>Welcome to React</h1>
