79df03d06ed6451cff9a0d96e3f111dacb21ac59
mithonk <mitho9798@gmail.com>
2024-06-05 14:59:38 +0530

    Add project files.

---
+++ backend-ASP-NET/Project-1/Models/Student.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Project_1.Models
+{
+    public class Student
+    {
+        [Key]
+        public int id { get; set; } 
+        public string name { get; set; }
+        public string course { get; set; }
+
+    }
+}
---
+++ backend-ASP-NET/Project-1/Models/StudentDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Project_1.Models
+{
+    public class StudentDbContext : DbContext
+    {
+        public StudentDbContext(DbContextOptions<StudentDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Student> Student { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=MITHONK\\SQLEXPRESS;Database=Project1;Trusted_Connection=True; TrustServerCertificate=True;");
+        }
+    }
+}
